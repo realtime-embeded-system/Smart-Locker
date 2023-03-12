9540ce88b91ecd93eeeba395d6752b39cabd22be
yuyang <123277203+realtime-embedded@users.noreply.github.com>
2023-03-12 20:10:20 +0000

    Add files via upload

---
+++ UI/code2.0/ctrl4.h
@@ -0,0 +1,25 @@
+#ifndef CTRL4_H
+#define CTRL4_H
+
+#include <QWidget>
+
+namespace Ui {
+class ctrl4;
+}
+
+class ctrl4 : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ctrl4(QWidget *parent = 0);
+    ~ctrl4();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::ctrl4 *ui;
+};
+
+#endif // CTRL4_H
