aca13dd555fadcbac4a7e88246b10f005746194a
LittleYuriCat <clannadcat@gmail.com>
2023-08-01 18:18:38 +0800

    change

--- build-deps.sh
+++ build-deps.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-cd  seetaFaceEngine/FaceDetection && mkdir build && cd build && cmake .. && make && cp -r include/* /usr/local/include && cp *.so /usr/local/lib &&cd -&& cd ..
-cd seetaFaceEngine/FaceAlignment && mkdir build && cd build && cmake .. && make && cp -r include/* /usr/local/include && cp *.so /usr/local/lib && cd - && cd ..
-cd seetaFaceEngine/FaceIdentification && mkdir build && cd build&&cmake .. && make && cp -r include/* /usr/local/include && cp *.so /usr/local/lib  && cd  - && cd ..
+cd  seetaFaceEngine/FaceDetection && mkdir build && cd build && cmake .. && make && cp -r ../include/* /usr/local/include && cp *.so /usr/local/lib &&cd -&& cd ..
+cd seetaFaceEngine/FaceAlignment && mkdir build && cd build && cmake .. && make && cp -r ../include/* /usr/local/include && cp *.so /usr/local/lib && cd - && cd ..
+cd seetaFaceEngine/FaceIdentification && mkdir build && cd build&&cmake .. && make && cp -r ../include/* /usr/local/include && cp *.so /usr/local/lib  && cd  - && cd ..
